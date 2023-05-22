62949746bf69732710f50056be104513f0ba5062
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-05-22 08:53:14 -0700

    styiling for adding fields with icons

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FieldArray,
+  FastField,
+} from "formik";
 import * as Yup from "yup";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 import TextError from "./TextError.js";
 
@@ -21,12 +28,12 @@ const initialValues = {
     facebook: "",
     twitter: "",
   },
-  phoneNumber: ["", ""],
+  phoneNumbers: ["", ""],
   phNumbers: [""],
 };
 
 const onSubmit = (values) => {
-  console.log(Formik.values);
+  console.log(values);
 };
 
 const YoutubeForm = () => {
@@ -38,24 +45,43 @@ const YoutubeForm = () => {
     >
       <Form>
         <div className="form-control">
-          <label htmlFor="name">Name</label>
-          <Field type="text" id="name" name="name" placeholder="Name" />
+          <label htmlFor="name">
+            Name<span className="error">*</span>
+          </label>
+          <Field
+            className="field-form"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="First and last name"
+          />
           <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
-          <label htmlFor="email">E-mail</label>
-          <Field type="email" id="email" name="email" placeholder="E-mail" />
+          <label htmlFor="email">
+            E-mail<span className="error">*</span>
+          </label>
+          <Field
+            className="field-form"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="E-mail address"
+          />
           <ErrorMessage name="email" component={TextError} />
         </div>
 
         <div className="form-control">
-          <label htmlFor="channel">Channel</label>
+          <label htmlFor="channel">
+            Channel<span className="error">*</span>
+          </label>
           <Field
             type="text"
             id="channel"
             name="channel"
-            placeholder="Channel Name"
+            className="field-form"
+            placeholder="Youtube channel name"
           />
           <ErrorMessage name="channel" component={TextError} />
         </div>
@@ -66,62 +92,73 @@ const YoutubeForm = () => {
             as="textarea"
             id="comments"
             name="comments"
+            className="field-form"
             placeholder="Leave a comment..."
           />
         </div>
 
         <div className="form-control">
           <label htmlFor="address">Address</label>
-          <Field name="address">
+          <FastField name="address">
             {(props) => {
               const { field, meta } = props;
               return (
                 <div>
-                  <input type="text" id="address" {...field} />
+                  <input
+                    className="field-form"
+                    type="text"
+                    id="address"
+                    placeholder="Mailing address"
+                    {...field}
+                  />
                   {meta.touched && meta.error ? <div>{meta.error}</div> : null}
                 </div>
               );
             }}
-          </Field>
+          </FastField>
         </div>
 
         <div className="form-control">
           <label htmlFor="facebook">Facebook Profile</label>
           <Field
+            className="field-form"
             type="text"
             id="facebook"
             name="social.facebook"
-            placeholder="Your profile"
+            placeholder="Your profile name"
           />
         </div>
 
         <div className="form-control">
           <label htmlFor="twitter">Twitter Profile</label>
           <Field
+            className="field-form"
             type="text"
             id="twitter"
             name="social.twitter"
-            placeholder="Your profile"
+            placeholder="Twitter handle"
           />
         </div>
 
         <div className="form-control">
           <label htmlFor="primaryPh">Primary phone number</label>
           <Field
+            className="field-form"
             type="text"
             id="primaryPh"
             name="phoneNumbers[0]"
-            placeholder="(XXX)XXX-XXXX"
+            placeholder="(XXX) XXX-XXXX"
           />
         </div>
 
         <div className="form-control">
           <label htmlFor="secondaryPh">Secondary phone number</label>
           <Field
+            className="field-form"
             type="text"
             id="secondaryPh"
             name="phoneNumbers[1]"
-            placeholder="(XXX)XXX-XXXX"
+            placeholder="(XXX) XXX-XXXX"
           />
         </div>
 
@@ -129,23 +166,29 @@ const YoutubeForm = () => {
           <label>List of phone numbers</label>
           <FieldArray name="phNumbers">
             {(fieldArrayProps) => {
-              console.log(fieldArrayProps);
               const { form, push, remove } = fieldArrayProps;
               const { values } = form;
               const { phNumbers } = values;
               return (
-                <div>
+                <div className="container">
                   {phNumbers.map((phNumber, index) => (
-                    <div key={index}>
-                      <Field name={`phNumbers[${index}]`} />
+                    <div key={index} className="field">
+                      <Field
+                        type="text"
+                        name={`phNumbers[${index}]`}
+                        placeholder="(XXX) XXX-XXXX"
+                        className="num-list"
+                      />
                       {index > 0 && (
-                        <button type="button" onClick={() => remove(index)}>
-                          <AiOutlineMinus />
-                        </button>
+                        <AiOutlineMinusCircle
+                          className="button remove"
+                          onClick={() => remove(index)}
+                        />
                       )}
-                      <button type="button" onClick={() => push("")}>
-                        <AiOutlinePlus />
-                      </button>
+                      <AiOutlinePlusCircle
+                        className="button add"
+                        onClick={() => push("")}
+                      />
                     </div>
                   ))}
                 </div>
